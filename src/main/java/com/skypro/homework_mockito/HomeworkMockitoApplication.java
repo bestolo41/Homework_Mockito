f68adc4e4944af9ad2e6386b08ceee6e7dcf57c4
BULAT <ibragimovbi@yandex.ru>
2023-02-16 20:24:16 +0300

    Initial commit

---
+++ src/main/java/com/skypro/homework_mockito/HomeworkMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.homework_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/homework_mockito/HomeworkMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.homework_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
